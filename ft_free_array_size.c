ebe87c2db55d7ad29ddd22649f2899a1a5ee83a1
Luis Balsa <luisbalsa@gmail.com>
2023-07-19 13:44:17 +0100

    Free array
    Free array size

--- ft_free_array_size.c
+++ ft_free_array_size.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_free_array.c                                    :+:      :+:    :+:   */
+/*   ft_free_array_void.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luide-so <luide-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 21:06:54 by luide-so          #+#    #+#             */
-/*   Updated: 2023/07/19 13:40:50 by luide-so         ###   ########.fr       */
+/*   Created: 2023/07/19 13:39:45 by luide-so          #+#    #+#             */
+/*   Updated: 2023/07/19 13:41:51 by luide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_free_array(char **array)
+void	ft_free_array_size(void **array, int size)
 {
 	int	i;
 
 	i = 0;
-	while (array[i])
+	while (i < size)
 		free(array[i++]);
 	free(array);
 }
