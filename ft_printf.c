bb4e17450496fe513dc08dac2337a3d654ad8be5
Luis Balsa <luisbalsa@gmail.com>
2024-04-24 03:13:57 +0100

    Fixed string==NULL case

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: luide-so <luide-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:25:34 by luide-so          #+#    #+#             */
-/*   Updated: 2023/06/25 13:04:15 by luide-so         ###   ########.fr       */
+/*   Updated: 2024/04/24 03:12:19 by luide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,8 @@ int	ft_printf(const char *param, ...)
 	int		flags[8];
 	va_list	ap;
 
+	if (!param)
+		return (-1);
 	va_start(ap, param);
 	count = 0;
 	k = 0;
